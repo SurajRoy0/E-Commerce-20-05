4be934ecd4310445f42cd5282f64f747470fc04d
unknown <myself.suraj0@gmail.com>
2023-05-24 20:44:29 +0530

    Contanct Info Added  With Firebase

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Cart from './Components/Cart/Cart';
 import Navbar from './Components/Header/NavBar';
 import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
+import ContactUs from './Components/Pages/ContactUs';
 
 function App() {
   const [isCartOpen, setIsCartOpen] = useState(false);
@@ -27,6 +28,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/Components/Pages/ContactUs.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "./ContactUs.module.css";
+
+const ContactUs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+  const sendContactDataHandler = async (data) => {
+    try {
+      const response = await axios.post(
+        "https://react-e-commerce-sharpener-default-rtdb.asia-southeast1.firebasedatabase.app/contact-info.json",
+        data
+      );
+      const responseData = await response.data;
+      console.log("Post request successful:", responseData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const nameInfo = name.trim();
+    const emailInfo = email.trim();
+    const phoneInfo = phone.trim();
+    const descriptionInfo = email.trim();
+    const contactData = { nameInfo, emailInfo, phoneInfo, descriptionInfo };
+    sendContactDataHandler(contactData);
+    setName("");
+    setEmail("");
+    setPhone("");
+    setDescription("");
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Contact Us</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="phone">Phone Number:</label>
+          <input
+            type="tel"
+            id="phone"
+            value={phone}
+            onChange={handlePhoneChange}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={handleDescriptionChange}
+            required
+          />
+        </div>
+        <button type="submit" className={styles.submitButton}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
